9c0b5ff22456646757cfd93c0f637f63a444d32b
xiaobai <18756096010@163.com>
2020-03-13 10:47:36 +0800

    模拟实现高并发秒杀

--- src/test/java/com/blj/bigDecimal/BigDecimalTest2.java
+++ src/test/java/com/blj/bigDecimal/BigDecimalTest2.java
@@ -73,4 +73,36 @@ public class BigDecimalTest2 {
     }
 
 
+    @Test
+    public void test6() {
+        BigDecimal bigDecimal = new BigDecimal("1234");
+        //输出结果：相反数，bigDecimal=-1234
+        System.out.println("相反数，bigDecimal="+bigDecimal.negate());
+    }
+
+    /**
+     * bigDecimal.equals:
+     *          将这个BigDecimal与指定的对象进行比较以获得相等性。与compareTo不同的是，
+     *           该方法只在两个BigDecimal对象的值和比例相等时才认为它们是相等的(因此，与此方法相比，2.0不等于2.00)
+     *
+     * 结论：
+     *          对于BigDecimal的大小比较，equals方法不仅比较值的大小，还比较两个对象的精确度，
+     *           而compareTo方法则不比较精确度，只比较数值的大小。
+     *
+     */
+    @Test
+    public void test7() {
+        BigDecimal bigDecimal = new BigDecimal("0.00");
+        final BigDecimal zero = BigDecimal.ZERO;
+        System.out.println("equals 方法比较结果为："+bigDecimal.equals(zero)); //false
+
+        if (bigDecimal.compareTo(zero)==0) {//true
+            System.out.println("二者相等");
+        }else {//false
+            System.out.println("二者不相等");
+        }
+
+
+    }
+
 }
