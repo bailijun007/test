cbd6633e4dd90c2aa6b6bb15e0d67406080c6280
bailijun <18756096010@163.com>
2020-06-12 22:48:15 +0800

    spi

---
+++ src/main/java/com/blj/spi/IParseFile.java
@@ -0,0 +1,9 @@
+package com.blj.spi;
+
+/**
+ * @author BaiLiJun on 2020/6/12
+ */
+public interface IParseFile {
+
+    void Parse();
+}
---
+++ src/main/java/com/blj/spi/ParseDoc.java
@@ -0,0 +1,11 @@
+package com.blj.spi;
+
+/**
+ * @author BaiLiJun on 2020/6/12
+ */
+public class ParseDoc implements IParseFile {
+    @Override
+    public void Parse() {
+        System.out.println("解析 Doc ");
+    }
+}
