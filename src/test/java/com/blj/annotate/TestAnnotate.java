932771dc6f20382eba8108e5bdeed8b2d0b1a9ae
“bailijun” <bailijunwb692@hellobike.com>
2020-07-22 16:05:45 +0800

    注释是不会执行，但是编译器读到之后会进行处理，转成 class 就凉凉了

--- src/test/java/com/blj/annotate/TestAnnotate.java
+++ src/test/java/com/blj/annotate/TestAnnotate.java
@@ -5,6 +5,7 @@ import javax.persistence.ManyToOne;
 /**
  * @author BaiLiJun on 2020/7/22 3:49 下午
  * 注释是不会执行，但是编译器读到之后会进行处理，转成 class 就凉凉了
+ * https://blog.csdn.net/qing_gee/article/details/105895395?utm_medium=distribute.pc_feed.none-task-blog-alirecmd-1.nonecase&depth_1-utm_source=distribute.pc_feed.none-task-blog-alirecmd-1.nonecase&request_id=
  */
 public class TestAnnotate {
     public static void main(String[] args) {
