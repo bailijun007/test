435c0d7175315c7aaed1575d4f9d4f25fde30892
“bailijun” <bailijunwb692@hellobike.com>
2020-08-18 12:09:26 +0800

    Java如何从HttpServletRequest中读取HTTP请求的body

--- src/main/java/com/blj/controller/UserController.java
+++ src/main/java/com/blj/controller/UserController.java
@@ -18,9 +18,13 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -108,6 +112,25 @@ public class UserController {
         User user = new UserInputDtoConvert().convert(inputDto);
         User saveUser = userService.saveUser(user);
         return saveUser;
+    }
+
+    /**
+     * servlet 获取 body Json数据（post 请求）
+     * https://blog.csdn.net/zxygww/article/details/47045055
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "postTest", method = RequestMethod.POST)
+    @ResponseBody
+    public String postTest(HttpServletRequest request) throws IOException {
+        BufferedReader br = request.getReader();
+        String str, wholeStr = "";
+        while((str = br.readLine()) != null){
+            wholeStr += str;
+        }
+        System.out.println(wholeStr);
+        return wholeStr;
     }
 
 
