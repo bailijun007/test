f334aee53a492f32b964fc8a9113564263692a1e
“bailijun” <bailijunwb692@hellobike.com>
2020-08-28 15:01:33 +0800

    在springAOP里面获取request和response对象

--- src/main/java/com/blj/proxy/aop/ControllerLogAspect.java
+++ src/main/java/com/blj/proxy/aop/ControllerLogAspect.java
@@ -5,13 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.assertj.core.util.Lists;
-import org.junit.rules.Stopwatch;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+import java.util.Enumeration;
+
 
 /**
  * @author bailj@linkstec.com
@@ -40,17 +40,51 @@ public class ControllerLogAspect {
     @Around("execution(* com.blj.controller.*.*(..))")
     public Object handleControllerMethod(ProceedingJoinPoint pjp) throws Throwable {
         long start = System.currentTimeMillis();
-        log.info("进入 :{} 方法中" , pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName());
+        log.info("==> 进入:{}方法中" , pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName()+"()");
+
+        // 获取请求头信息
+         //getRequestHeaderInfo();
 
         //获取参数，pre操作
-        log.info("收到参数为：{}", Arrays.toString(pjp.getArgs()));
+        log.info("==> 收到参数为:{}", Arrays.toString(pjp.getArgs()));
 
         Object result = pjp.proceed(pjp.getArgs());
-        log.info("返回值为：{}", JSON.toJSONString(result));
+        log.info("==> 返回值为:{}", JSON.toJSONString(result));
         long end = System.currentTimeMillis();
-        log.info("消耗时间：{}毫秒", (end - start));
+        log.info("==> 消耗时间:{}毫秒", (end - start));
         return result;
     }
 
+
+    /**
+     * 获取请求头信息
+     */
+    private void getRequestHeaderInfo(){
+        // 在springAOP里面获取request和response对象
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+//        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+
+        log.info("----------request.getRequestURI()="+ request.getRequestURI()+"---------------");
+
+        // TODO 注意:request.getQueryString()只能获取到get请求方式到参数
+        log.info("----------request.getQueryString()="+ request.getQueryString()+"---------------");
+
+        // TODO 注意: request.getParameterNames()只能获取到get请求方式到参数
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while(parameterNames.hasMoreElements()){
+            log.info("----------parameterNames.nextElement()="+ parameterNames.nextElement().toString() +"---------------");
+        }
+
+        Enumeration<String> names =  request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            log.info("----------request.getHeaderNames().nextElement()="+ names.nextElement().toString()+"---------------");
+        }
+
+        log.info("----------request.getHeader(Content-Type)="+ request.getHeader("Content-Type")+"---------------");
+
+        log.info("----------request.getHeader(host)="+ request.getHeader("host")+"---------------");
+
+
+    }
 
 }
--- src/test/java/com/blj/java8/stream/CollectorsTest.java
+++ src/test/java/com/blj/java8/stream/CollectorsTest.java
@@ -1,9 +1,11 @@
 package com.blj.java8.stream;
 
 import com.alibaba.fastjson.JSON;
+import com.blj.mapper.UserMapper;
 import com.blj.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,24 +16,26 @@ import java.util.stream.Collectors;
 
 /**
  * @author BaiLiJun on 2020/7/17 5:14 下午
- *
+ * <p>
  * Collectors.toList();
  * Collectors.toMap();
  * Collectors.toSet();
  * Collectors.toCollection();
  * Collectors.toConcurrentMap();
- *
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("local")
 public class CollectorsTest {
 
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * 该方法先执行了一个归纳操作，然后再对归纳的结果进行 Function 函数处理输出一个新的结果
      */
     @Test
-    public void testCollectingAndThen(){
+    public void testCollectingAndThen() {
         List<User> list = getList();
         // 比如我们将servers joining 然后转成大写
         String s = list.stream().map(User::getName).collect(Collectors.collectingAndThen(Collectors.joining(","), String::toUpperCase));
@@ -41,12 +45,22 @@ public class CollectorsTest {
 
 
     @Test
-    public void testGroupingBy(){
+    public void testGroupingBy() {
         List<User> list = getList();
 
         // 按年龄分组
-        Map<Integer, List<User>> listMap = list.stream().collect(Collectors.groupingBy(User::getAge));
+        Map<Integer, List<User>> listMap = list.stream().filter(u->u.getAge()!=null).collect(Collectors.groupingBy(User::getAge));
         System.out.println("按年龄分组 ： " + JSON.toJSONString(listMap));
+
+        for (Integer key : listMap.keySet()) {
+            List<User> list1 = listMap.get(key);
+            List<User> userList = list1.stream().filter(u -> u.getCreateDate() != null)
+                    .sorted((d1, d2) -> d2.getCreateDate().compareTo(d1.getCreateDate()))
+                    .limit(3)
+                    .collect(Collectors.toList());
+            System.out.println("userList = " + userList);
+
+        }
 
         // 如果我不想 Map 的 value 为 List 需要set怎么做呢
         Map<Integer, Set<User>> setMap = list.stream().collect(Collectors.groupingBy(User::getAge, Collectors.toSet()));
@@ -60,16 +74,15 @@ public class CollectorsTest {
     }
 
 
-
-
-    public List<User>  getList(){
-        List<User> list = Arrays.asList(
-                new User(1L, "zhangsan", 29, "123456"),
-                new User(2L, "lisi", 25, "123456"),
-                new User(3L, "zhaoliu", 25, "123456"),
-                new User(4L, "wangerma", 34, "123456"),
-                new User(5L, "zhangergou", 28, "12345")
-        );
+    public List<User> getList() {
+//        List<User> list = Arrays.asList(
+//                new User(1L, "zhangsan", 29, "123456"),
+//                new User(2L, "lisi", 25, "123456"),
+//                new User(3L, "zhaoliu", 25, "123456"),
+//                new User(4L, "wangerma", 34, "123456"),
+//                new User(5L, "zhangergou", 28, "12345")
+//        );
+        List<User> list = userMapper.findAll();
         return list;
     }
 
